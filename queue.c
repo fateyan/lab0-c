22fe7775beaf46f396a52a8041bfd3f1ca91d250
fateyan <fateyan.tw@gmail.com>
2019-03-03 22:46:21 +0800

    Check if malloc in new_q returned NULL

--- queue.c
+++ queue.c
@@ -26,6 +26,8 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
+    if (q == NULL)
+        return NULL;
     q->head = NULL;
     return q;
 }
